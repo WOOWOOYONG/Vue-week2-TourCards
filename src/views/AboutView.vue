c9125b1bc5eb6cbef57afe0a2cee0dcab34da29a
woowooyong <woowooyong@gmail.com>
2023-04-25 17:32:52 +0800

    initial

---
+++ src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<script>
+import TourWrapper from '../components/TourWrapper.vue'
+
+export default {
+  components: {
+    TourWrapper
+  }
+}
+</script>
+
+<template>
+  <h1>About Page</h1>
+  <TourWrapper />
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+  margin-bottom: 2rem;
+  font-weight: 600;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script>
+import TourWrapper from '../components/TourWrapper.vue'
+
+export default {
+  components: {
+    TourWrapper
+  }
+}
+</script>
+
+<template>
+  <h1>Home Page</h1>
+  <TourWrapper />
+</template>
+<style scoped>
+h1 {
+  text-align: center;
+  margin-bottom: 2rem;
+  font-weight: 600;
+}
+</style>
