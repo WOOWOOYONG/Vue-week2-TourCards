fa7d766441bdf6348c080bd934529d0b89b80c6d
woowooyong <woowooyong@gmail.com>
2023-05-09 22:24:08 +0800

    update: add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:woowooyong/Vue-week2-TourCards.git main:gh-pages
+
+cd -
